d3057c4fce6e6fd13e738d517d66a34f9aa1182b
alberto sartori <alberto.sartori86@gmail.com>
2016-06-28 13:53:41 +0200

    back to two

--- include/interfaces/two_phase_flow.h
+++ include/interfaces/two_phase_flow.h
@@ -81,9 +81,10 @@ private:
       return ret;
   }
 
-  double smooth(const double n1, const double n2, const double c) const
+  template <typename number>
+  double smooth(const double n1, const double n2, const number c) const
   {
-    double c_half = c-0.5;
+    double c_half = to_double(c)-0.5;
     double n = (n1 - n2)*He(c_half)+n2;
     return n;
   }
@@ -105,9 +106,9 @@ private:
 template <int dim, typename LAC>
 ThreePhaseFlow<dim,LAC>::ThreePhaseFlow() :
   PDESystemInterface<dim, dim, ThreePhaseFlow<dim,LAC>, LAC>("Three phase flows",
-                                                     dim+7,1,
-                                                     "FESystem[FE_Q(2)^d-FE_Q(1)-FE_Q(1)-FE_Q(1)-FE_Q(1)-FE_Q(1)-FE_Q(1)-FE_Q(1)]",
-                                                     "u,u,p,c1,c2,c3,mu1,mu2,mu3","1,0,1,1,1,0,0,0")
+                                                     dim+3,1,
+                                                     "FESystem[FE_Q(2)^d-FE_Q(1)-FE_Q(1)-FE_Q(1)]",
+                                                     "u,u,p,c1,mu1","1,0,1,0")
 {}
 
 
@@ -127,11 +128,7 @@ energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cel
     const FEValuesExtractors::Vector velocity(0);
     const FEValuesExtractors::Scalar pressure(dim);
     const FEValuesExtractors::Scalar FEc1(dim+1);
-    const FEValuesExtractors::Scalar FEc2(dim+2);
-    const FEValuesExtractors::Scalar FEc3(dim+3);
-    const FEValuesExtractors::Scalar aux1(dim+4);
-    const FEValuesExtractors::Scalar aux2(dim+5);
-    const FEValuesExtractors::Scalar aux3(dim+6);
+    const FEValuesExtractors::Scalar aux1(dim+2);
 
 
     auto &us_dot = fe_cache.get_values("solution_dot", "velocity", velocity, alpha);
@@ -147,26 +144,12 @@ energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cel
     auto &c1s = fe_cache.get_values("solution", "c", FEc1, alpha);
     auto &grad_c1s = fe_cache.get_gradients("solution", "c", FEc1, alpha);
 
-    auto &c2s_dot = fe_cache.get_values("solution_dot", "c", FEc2, alpha);
-    auto &c2s = fe_cache.get_values("solution", "c", FEc2, alpha);
-    auto &grad_c2s = fe_cache.get_gradients("solution", "c", FEc2, alpha);
-
-    auto &c3s_dot = fe_cache.get_values("solution_dot", "c", FEc3, alpha);
-    auto &c3s = fe_cache.get_values("solution", "c", FEc3, alpha);
-    auto &grad_c3s = fe_cache.get_gradients("solution", "c", FEc3, alpha);
-
     auto &mu1s = fe_cache.get_values("solution", "mu1", aux1, alpha);
     auto &grad_mu1s = fe_cache.get_gradients("solution", "mu1", aux1, alpha);
-    auto &mu2s = fe_cache.get_values("solution", "mu2", aux2, alpha);
-    auto &grad_mu2s = fe_cache.get_gradients("solution", "mu2", aux2, alpha);
-    auto &mu3s = fe_cache.get_values("solution", "mu3", aux3, alpha);
-    auto &grad_mu3s = fe_cache.get_gradients("solution", "mu3", aux3, alpha);
 
     double dd;
     auto &us_expl = fe_cache.get_values("explicit_solution", "velocity", velocity, dd);
     auto &c1s_expl = fe_cache.get_values("explicit_solution", "conc1", FEc1, dd);
-    auto &c2s_expl = fe_cache.get_values("explicit_solution", "conc2", FEc2, dd);
-    auto &c3s_expl = fe_cache.get_values("explicit_solution", "conc3", FEc3, dd);
 
     const unsigned int n_q_points = c1s.size();
 
@@ -179,85 +162,49 @@ energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cel
     for (unsigned int q=0; q<n_q_points; ++q)
       {
         const ResidualType &c1 = c1s[q];
-        const ResidualType &c2 = c2s[q];
-        const ResidualType &c3 = c3s[q];
         const ResidualType &mu1 = mu1s[q];
-        const ResidualType &mu2 = mu2s[q];
-        const ResidualType &mu3 = mu3s[q];
 
         const double &c1_e = c1s_expl[q];
-        const double &c2_e = c2s_expl[q];
-        const double &c3_e = c3s_expl[q];
 
         const Tensor<1,dim,ResidualType> &grad_c1 = grad_c1s[q];
-        const Tensor<1,dim,ResidualType> &grad_c2 = grad_c2s[q];
-        const Tensor<1,dim,ResidualType> &grad_c3 = grad_c3s[q];
         const Tensor<1,dim,ResidualType> &grad_mu1 = grad_mu1s[q];
-        const Tensor<1,dim,ResidualType> &grad_mu2 = grad_mu2s[q];
-        const Tensor<1,dim,ResidualType> &grad_mu3 = grad_mu3s[q];
 
         const ResidualType &c1_dot = c1s_dot[q];
-        const ResidualType &c2_dot = c2s_dot[q];
-        const ResidualType &c3_dot = c3s_dot[q];
 
-        double rho = smooth(rho1, rho2, rho3, c1, c2);
-        double eta = smooth(eta1, eta2, eta3, c1, c2);
+//        double rho = smooth(rho1, rho2, rho3, c1, c2);
+//        double eta = smooth(eta1, eta2, eta3, c1, c2);
+        double rho = smooth(rho1, rho2, c1);
+        double eta = smooth(eta1, eta2, c1);
 
         for (unsigned int i=0; i<local_residuals[0].size(); ++i)
           {
             // cahn-hilliard
             auto test_c1 = fev[FEc1].value(i,q);
-            auto test_c2 = fev[FEc2].value(i,q);
-            auto test_c3 = fev[FEc3].value(i,q);
 
             auto grad_test_c1 = fev[FEc1].gradient(i,q);
-            auto grad_test_c2 = fev[FEc2].gradient(i,q);
-            auto grad_test_c3 = fev[FEc3].gradient(i,q);
 
             auto test_mu1 = fev[aux1].value(i,q);
-            auto test_mu2 = fev[aux2].value(i,q);
-            auto test_mu3 = fev[aux3].value(i,q);
             auto grad_test_mu1 = fev[aux1].gradient(i,q);
-            auto grad_test_mu2 = fev[aux2].gradient(i,q);
-            auto grad_test_mu3 = fev[aux3].gradient(i,q);
 
             local_residuals[0][i] += (
                   c1_dot*test_c1
-                  + M0/Sigma1*scalar_product(grad_mu1,grad_test_c1)
+                  + M0*scalar_product(grad_mu1,grad_test_c1)
                   + scalar_product(us_expl[q],grad_c1)*test_c1
                   + mu1*test_mu1
-                  - (2.*sigma12*c1*c2*c2 + 2.*sigma13*c1*c3*c3
-                     + c2*c3*(Sigma1*c1+Sigma2*c2+Sigma3*c3) + c1*c2*c3*Sigma1
-                     +2./3.*Lambda*c1*(c2_e*c2_e*c3_e*c3_e
-                                       + 0.5*c2*c2*c3_e*c3_e
-                                       + 0.5*c2_e*c2_e*c3*c3
-                                       + c2*c2*c3*c3))*test_mu1
-                  - 3./4.*Sigma1*eps*scalar_product(grad_c1,grad_test_mu1)
-
-                  + c2_dot*test_c2
-                  + M0/Sigma2*scalar_product(grad_mu2,grad_test_c2)
-                  + scalar_product(us_expl[q],grad_c2)*test_c2
-                  + mu2*test_mu2
-                  - (2.*sigma12*c1*c1*c2 + 2.*sigma23*c2*c3*c3
-                     + c1*c3*(Sigma1*c1+Sigma2*c2+Sigma3*c3) + c1*c2*c3*Sigma2
-                     +2./3.*Lambda*c2*(c1_e*c1_e*c3_e*c3_e
-                                       + 0.5*c1*c1*c3_e*c3_e
-                                       + 0.5*c1_e*c1_e*c3*c3
-                                       + c1*c1*c3*c3))*test_mu2
-                  - 3./4.*Sigma2*eps*scalar_product(grad_c2,grad_test_mu2)
+                  - 3./2.*sigma12*eps*scalar_product(grad_c1,grad_test_mu1)
+                  - 24.*sigma12/eps*c1*(1.-c1)*(1.-2.*c1)
 
 
-                  + c3_dot*test_c3
-                  + M0/Sigma3*scalar_product(grad_mu3,grad_test_c3)
-                  + scalar_product(us_expl[q],grad_c3)*test_c3
-                  + mu3*test_mu3
-                  - (2.*sigma13*c1*c1*c3 + 2.*sigma23*c2*c2*c3
-                     + c1*c2*(Sigma1*c1+Sigma2*c2+Sigma3*c3) + c1*c2*c3*Sigma3
-                     +2./3.*Lambda*c3*(c1_e*c1_e*c2_e*c2_e
-                                       + 0.5*c1*c1*c2_e*c2_e
-                                       + 0.5*c1_e*c1_e*c2*c2
-                                       + c1*c1*c2*c2))*test_mu3
-                  - 3./4.*Sigma3*eps*scalar_product(grad_c3,grad_test_mu3)
+//                  - (2.*sigma12*c1*(1.-c1)*(1.-c1))*test_mu1
+//                  - 3./4.*Sigma1*eps*scalar_product(grad_c1,grad_test_mu1)
+
+//                  + c2_dot*test_c2
+//                  + M0/Sigma2*scalar_product(grad_mu2,grad_test_c2)
+//                  + scalar_product(us_expl[q],grad_c2)*test_c2
+//                  + mu2*test_mu2
+//                  - (2.*sigma12*c1*c1*c2)*test_mu2
+//                  - 3./4.*Sigma2*eps*scalar_product(grad_c2,grad_test_mu2)
+
                     )*JxW[q];
 
             // NS
@@ -296,8 +243,7 @@ energies_and_residuals(const typename DoFHandler<dim>::active_cell_iterator &cel
                                     -
                                     rho*(g*test_v)
                   - scalar_product((mu1*grad_c1),test_v)
-                  - scalar_product((mu2*grad_c2),test_v)
-                  - scalar_product((mu3*grad_c3),test_v)
+//                  - scalar_product((mu2*grad_c2),test_v)
 
                                    )*JxW[q];
             local_residuals[0][i] += r;
