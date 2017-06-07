230d0dd3c55e9b3d32af5971351f334ce808e486
asartori86 <alberto.sartori86@gmail.com>
2017-06-07 17:29:17 +0200

    first working version

--- include/interfaces/entanglement.h
+++ include/interfaces/entanglement.h
@@ -40,78 +40,78 @@ public:
                               std::vector<std::vector<ResidualType> > &local_residuals,
                               bool compute_only_system_terms) const;
 
-  virtual void connect_to_signals() const
-  {
-    // first of all we get the struct Signals from pidomus
-    auto &signals = this->get_signals();
+//  virtual void connect_to_signals() const
+//  {
+//    // first of all we get the struct Signals from pidomus
+//    auto &signals = this->get_signals();
 
-    // we can connect calling .connect( and defining a lambda
-    signals.fix_initial_conditions.connect(
-      [this](typename LAC::VectorType &, typename LAC::VectorType &)
-    {
-      std::cout << "ciao mondo" << std::endl;
-    }
-    );
+//    // we can connect calling .connect( and defining a lambda
+//    signals.fix_initial_conditions.connect(
+//      [this](typename LAC::VectorType &, typename LAC::VectorType &)
+//    {
+//      std::cout << "ciao mondo" << std::endl;
+//    }
+//    );
 
-    // or we can define a lambda first
-    auto l =  [this](typename LAC::VectorType &, typename LAC::VectorType &)
-    {
-      std::cout << "ho raffinato" << std::endl;
-    };
+//    // or we can define a lambda first
+//    auto l =  [this](typename LAC::VectorType &, typename LAC::VectorType &)
+//    {
+//      std::cout << "ho raffinato" << std::endl;
+//    };
 
-    // and then attach the just defined lambda
-    signals.fix_solutions_after_refinement.connect(l);
+//    // and then attach the just defined lambda
+//    signals.fix_solutions_after_refinement.connect(l);
 
 
-    // herebelow, we connect to all the begin_* signals available in piDoMUS
-    auto &pcout = this->get_pcout();
-    signals.begin_make_grid_fe.connect(
-      [&]()
-    {
-      pcout << "#########  make_grid_fe"<<std::endl;
-    });
-    signals.begin_setup_dofs.connect(
-      [&]()
-    {
-      pcout << "#########  setup_dofs"<<std::endl;
-    });
-    signals.begin_refine_mesh.connect(
-      [&]()
-    {
-      pcout << "#########  refine_mesh"<<std::endl;
-    });
-    signals.begin_setup_jacobian.connect(
-      [&]()
-    {
-      pcout << "#########  setup_jacobian"<<std::endl;
-    });
-    signals.begin_residual.connect(
-      [&]()
-    {
-      pcout << "#########  residual"<<std::endl;
-    });
-    signals.begin_solve_jacobian_system.connect(
-      [&]()
-    {
-      pcout << "#########  solve_jacobian_system"<<std::endl;
-    });
-    signals.begin_refine_and_transfer_solutions.connect(
-      [&]()
-    {
-      pcout << "#########  refine_and_transfer_solutions"<<std::endl;
-    });
-    signals.begin_assemble_matrices.connect(
-      [&]()
-    {
-      pcout << "#########  assemble_matrices"<<std::endl;
-    });
-    signals.begin_solver_should_restart.connect(
-      [&]()
-    {
-      pcout << "#########  solver_should_restart"<<std::endl;
-    });
+//    // herebelow, we connect to all the begin_* signals available in piDoMUS
+//    auto &pcout = this->get_pcout();
+//    signals.begin_make_grid_fe.connect(
+//      [&]()
+//    {
+//      pcout << "#########  make_grid_fe"<<std::endl;
+//    });
+//    signals.begin_setup_dofs.connect(
+//      [&]()
+//    {
+//      pcout << "#########  setup_dofs"<<std::endl;
+//    });
+//    signals.begin_refine_mesh.connect(
+//      [&]()
+//    {
+//      pcout << "#########  refine_mesh"<<std::endl;
+//    });
+//    signals.begin_setup_jacobian.connect(
+//      [&]()
+//    {
+//      pcout << "#########  setup_jacobian"<<std::endl;
+//    });
+//    signals.begin_residual.connect(
+//      [&]()
+//    {
+//      pcout << "#########  residual"<<std::endl;
+//    });
+//    signals.begin_solve_jacobian_system.connect(
+//      [&]()
+//    {
+//      pcout << "#########  solve_jacobian_system"<<std::endl;
+//    });
+//    signals.begin_refine_and_transfer_solutions.connect(
+//      [&]()
+//    {
+//      pcout << "#########  refine_and_transfer_solutions"<<std::endl;
+//    });
+//    signals.begin_assemble_matrices.connect(
+//      [&]()
+//    {
+//      pcout << "#########  assemble_matrices"<<std::endl;
+//    });
+//    signals.begin_solver_should_restart.connect(
+//      [&]()
+//    {
+//      pcout << "#########  solver_should_restart"<<std::endl;
+//    });
 
-  }
+//  }
 
 
 
@@ -151,16 +151,19 @@ EntanglementInterface<dim,spacedim,LAC>::
 energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iterator &cell,
                        FEValuesCache<dim,spacedim> &fe_cache,
                        std::vector<EnergyType> &energies,
-                       std::vector<std::vector<ResidualType> > &/*local_residuals*/,
+                       std::vector<std::vector<ResidualType> > &local_residuals,
                        bool ) const
 {
   const FEValuesExtractors::Vector u(0);
   {
     EnergyType et = 0; // dummy number to define the type of variables
+    ResidualType rt=0;
     double dut=0;
     this->reinit (et, cell, fe_cache);
+    fe_cache.cache_local_solution_vector("solution_dot",this->get_locally_relevant_solution_dot(),rt);
     auto &eus = fe_cache.get_values("explicit_solution", "u", u, dut);
     auto &us = fe_cache.get_values("solution", "u", u, et);
+    auto &uts = fe_cache.get_values("solution_dot", "ut", u, rt);
     auto &grad_us = fe_cache.get_gradients("solution", "grad_u",u, et);
 
     const unsigned int n_q_points = us.size();
@@ -168,7 +171,7 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
     auto &jacs = fe_cache.get_current_fe_values().get_jacobians();
     auto fev = dynamic_cast<const FEValues<dim,spacedim> *>(&(fe_cache.get_current_fe_values()));
     Assert(fev != nullptr, ExcInternalError());
-
+    auto &mfev = fe_cache.get_current_fe_values();
     auto &points = fev->get_quadrature_points();
 
     for (unsigned int q=0; q<n_q_points; ++q)
@@ -186,15 +189,21 @@ energies_and_residuals(const typename DoFHandler<dim,spacedim>::active_cell_iter
               for (unsigned int j=0; j<spacedim; ++j)
                 X[i][a] += jac[j][a]*gradu[i][j];
             }
-
+//        static const double eps = 1e-1;
         EnergyType z = p[spacedim-1]+uz;
         EnergyType psi = d2kinternal::determinant(X)/(z*z);
 //        EnergyType psi = d2kinternal::determinant(X);
 
+        EnergyType stab = scalar_product(gradu,gradu);
         double W = fev->get_quadrature().weight(q);
 
-        energies[0] += (psi*W);
+        energies[0] += (psi*W +stab*JxW[q]);
 
+//  for (unsigned int i=0; i<local_residuals[0].size(); ++i)
+//    {
+//      auto v = mfev[u].value(i,q);
+//      local_residuals[0][i] += (uts[q]*v)*JxW[q];
+//    }
 
       }
 
--- source/base_interface.cc
+++ source/base_interface.cc
@@ -260,7 +260,8 @@ assemble_local_system_residual (const typename DoFHandler<dim,spacedim>::active_
                                   residuals,
                                   true);
 
-  std::cout << "Energy = " << SacadoTools::to_double(energies[0]) << std::endl;
+//  auto &pcout = this->get_pcout();
+//  pcout << "Energy = " << Utilities::MPI::sum(SacadoTools::to_double(energies[0]), this->get_mpi_communicator()) << std::endl;
 
 
   for (unsigned int i=0; i<dofs_per_cell; ++i)
